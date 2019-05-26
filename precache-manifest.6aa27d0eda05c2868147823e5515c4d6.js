818197e47325c2e525e02e86d3e7c459fda858cc
braianchincho <braianchincho@gmail.com>
2019-05-26 19:21:54 -0300

    build file de climas react

---
+++ precache-manifest.6aa27d0eda05c2868147823e5515c4d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08211628579032b374b65de61a2a073",
+    "url": "/index.html"
+  },
+  {
+    "revision": "002e00422a8626555870",
+    "url": "/static/css/main.0010486f.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "002e00422a8626555870",
+    "url": "/static/js/main.b350ed57.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
