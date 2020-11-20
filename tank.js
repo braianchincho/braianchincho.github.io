3ffbdacbaed15eb061422c2c2d03775d4c353e11
braianchincho <braianchincho@gmail.com>
2020-11-20 11:16:12 -0300

    deploy tank defender game

---
+++ tank.js
@@ -0,0 +1,28 @@
+class Tank {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    paint(context) {
+        context.fillStyle = "rgba(200,0,0,1)"
+        context.beginPath();
+        context.rect(this.x,this.y,50,50);
+        context.closePath();
+        context.fill();
+    }
+    move(keyEvent, context) {
+        if (!keyEvent || !context ) { return; }
+        if (keyEvent.key === 'ArrowRight') {
+            this.x += 10;
+        } else if (keyEvent.key === 'ArrowLeft') {
+            this.x -= 10;
+        }
+        this.x = this.x <= 0 ? 0 : this.x;
+        this.x = this.x + 50 >= context.canvas.width ? context.canvas.width -50 : this.x;
+    }
+    shoot(keyEvent) {
+        if(keyEvent.key === ' '){
+            return new Bullet(this.x + 25, this.y);
+        }
+    }
+}
